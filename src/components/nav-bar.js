bcef21de4ff8f0d773dbc8c01336b71b1f7c1084
James Dong <45552485+Dongongngg@users.noreply.github.com>
2020-08-23 15:46:02 +1000

    aa

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -12,9 +12,9 @@ const NavBar = () => {
       </div>
       <div class="container">
         <div class="row">
-          <div class="col-sm">One of three columns</div>
-          <div class="col-sm">One of three columns</div>
-          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">1 of three columns</div>
+          <div class="col-sm">2 of three columns</div>
+          <div class="col-sm">3 of three columns</div>
         </div>
         <div class="row">
           <div class="col-8">col-8</div>
