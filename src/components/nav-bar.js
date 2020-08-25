7090a62c1d452a37a73199c5895656dfca5b3cb7
James Dong <45552485+Dongongngg@users.noreply.github.com>
2020-08-25 22:00:09 +1000

    navbar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/nav-bar.css";
 
 const NavBar = () => {
+  const [showCollapsedMenu, setShowCollapsedMenu] = useState(false);
+  const handleNavBar = () => {
+    setShowCollapsedMenu(!showCollapsedMenu);
+  };
+  const navOpen = (showCollapsedMenu && "show") || "";
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link active" href="#">
-              James <span className="sr-only">(current)</span>
-            </a>
-            <a className="nav-link" href="#">
-              Intro
-            </a>
-            <a className="nav-link" href="#">
-              Projects
-            </a>
-          </div>
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          onClick={handleNavBar}
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={"collapse navbar-collapse " + navOpen} id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                James Dong <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Project
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </React.Fragment>
